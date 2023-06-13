780e9a744c9cafda51d777cb115b843a05d0c188
Your Github username <d.pope@eagles.oc.edu>
2023-06-12 22:12:06 -0500

    404 error

--- app/controllers/itineraryStep.controller.js
+++ app/controllers/itineraryStep.controller.js
@@ -4,21 +4,19 @@ const ItineraryActivity = db.itineraryActivity;
 const Activity = db.activity;
 const ItineraryFlight = db.itineraryFlight;
 const Flight = db.flight;
-const ItineraryHotel = db.itineraryHotel
+const ItineraryHotel = db.itineraryHotel;
 const Hotel = db.hotel;
-const Step = db.Step;
+const Step = db.step; // Corrected casing
 const Op = db.Sequelize.Op;
+
 // Create and Save a new ItineraryStep
-exports.create = (req, res) => {
+exports.create = async (req, res) => {
   // Validate request
-  if (req.body.stepNumber === undefined) {
-    const error = new Error("Step Number cannot be empty for Itinerary Step!");
-    error.statusCode = 400;
-    throw error;
-  } else if (req.body.itineraryId === undefined) {
-    const error = new Error("Itinerary ID cannot be empty for Itinerary Step!");
-    error.statusCode = 400;
-    throw error;
+  if (!req.body.stepNumber) {
+    return res.status(400).send({message: "Step Number cannot be empty for Itinerary Step!"});
+  }
+  if (!req.body.itineraryId) {
+    return res.status(400).send({message: "Itinerary ID cannot be empty for Itinerary Step!"});
   }
 
   // Create a ItineraryStep
@@ -26,252 +24,215 @@ exports.create = (req, res) => {
     stepNumber: req.body.stepNumber,
     itineraryId: req.body.itineraryId,
   };
-  
+
   // Save itineraryStep in the database
-  ItineraryStep.create(itineraryStep)
-    .then((data) => {
-      res.send(data);
-    })
-    .catch((err) => {
-      res.status(500).send({
-        message:
-          err.message ||
-          "Some error occurred while creating the itineraryStep.",
-      });
+  try {
+    const data = await ItineraryStep.create(itineraryStep);
+    res.send(data);
+  } catch (err) {
+    res.status(500).send({
+      message: err.message || "Some error occurred while creating the itineraryStep.",
     });
+  }
 };
 
 // Retrieve all itinerarySteps from the database.
-exports.findAll = (req, res) => {
+exports.findAll = async (req, res) => {
   const itineraryStepId = req.query.itineraryStepId;
-  var condition = itineraryStepId
-    ? {
-        id: {
-          [Op.like]: `%${itineraryStepId}%`,
-        },
-      }
-    : null;
+  var condition = itineraryStepId ? { id: itineraryStepId } : null; // Fixed operator
 
-  ItineraryStep.findAll({ where: condition })
-    .then((data) => {
-      res.send(data);
-    })
-    .catch((err) => {
-      res.status(500).send({
-        message:
-          err.message ||
-          "Some error occurred while retrieving itinerarySteps.",
-      });
+  try {
+    const data = await ItineraryStep.findAll({ where: condition });
+    res.send(data);
+  } catch (err) {
+    res.status(500).send({
+      message: err.message || "Some error occurred while retrieving itinerarySteps.",
     });
+  }
 };
 
-exports.findAllForItinerary = (req, res) => {
+exports.findAllForItinerary = async (req, res) => {
   const itineraryId = req.params.itineraryId;
-  ItineraryStep.findAll({
-    where: { itineraryId: itineraryId },
-    include: [
-      {
-        model: Step,
-        as: "Step",
-        required: true,
-      },
-    ],
-  })
-    .then((data) => {
-      res.send(data);
-    })
-
-    .catch((err) => {
-      res.status(500).send({
-        message:
-          err.message ||
-          "Some error occurred while retrieving itinerarySteps for a itinerary.",
-      });
+  try {
+    const data = await ItineraryStep.findAll({
+      where: { itineraryId: itineraryId },
+      include: [
+        {
+          model: Step,
+          as: "step",
+          required: true,
+        },
+      ],
     });
+    res.send(data);
+  } catch (err) {
+    res.status(500).send({
+      message:
+        err.message ||
+        "Some error occurred while retrieving itinerarySteps for an itinerary.",
+    });
+  }
 };
 
-// Find all itinerarySteps for a itinerary and include the Activities
-exports.findAllForItineraryWithActivities = (req, res) => {
+exports.findAllForItineraryWithActivities = async (req, res) => {
   const itineraryId = req.query.itineraryId;
-  ItineraryStep.findAll({
-    where: { itineraryId: itineraryId },
-    include: [
-      {
-        model: ItineraryActivity,
-        as: "itineraryActivity",
-        required: false,
-        include: [
-          {
-            model: Activity,
-            as: "activity",
-            required: false,
-          }
-        ]
-      },
-    ],
-    order: [["stepNumber", 'ASC']],
-  })
-    .then((data) => {
-      res.send(data);
-    })
-    .catch((err) => {
-      res.status(500).send({
-        message:
-          err.message ||
-          "Some error occurred while retrieving itineraryActivities for a itinerary step.",
-      });
+  try {
+    const data = await ItineraryStep.findAll({
+      where: { itineraryId: itineraryId },
+      include: [
+        {
+          model: ItineraryActivity,
+          as: "itineraryActivity",
+          required: false,
+          include: [
+            {
+              model: Activity,
+              as: "activity",
+              required: false,
+            },
+          ],
+        },
+      ],
+      order: [["stepNumber", "ASC"]],
     });
+    res.send(data);
+  } catch (err) {
+    res.status(500).send({
+      message:
+        err.message ||
+        "Some error occurred while retrieving itineraryActivities for an itinerary step.",
+    });
+  }
 };
 
-// Find all itinerarySteps for a itinerary and include the Flights
-exports.findAllForItineraryWithFlights = (req, res) => {
+exports.findAllForItineraryWithFlights = async (req, res) => {
   const itineraryId = req.query.itineraryId;
-  ItineraryStep.findAll({
-    where: { itineraryId: itineraryId },
-    include: [
-      {
-        model: ItineraryFlight,
-        as: "itineraryFlight",
-        required: false,
-        include: [
-          {
-            model: Flight,
-            as: "flight",
-            required: false,
-          }
-        ]
-      },
-    ],
-    order: [["stepNumber", 'ASC']],
-  })
-    .then((data) => {
-      res.send(data);
-    })
-    .catch((err) => {
-      res.status(500).send({
-        message:
-          err.message ||
-          "Some error occurred while retrieving itineraryFlights for a itinerary step.",
-      });
+  try {
+    const data = await ItineraryStep.findAll({
+      where: { itineraryId: itineraryId },
+      include: [
+        {
+          model: ItineraryFlight,
+          as: "itineraryFlight",
+          required: false,
+          include: [
+            {
+              model: Flight,
+              as: "flight",
+              required: false,
+            },
+          ],
+        },
+      ],
+      order: [["stepNumber", "ASC"]],
     });
+    res.send(data);
+  } catch (err) {
+    res.status(500).send({
+      message:
+        err.message ||
+        "Some error occurred while retrieving itineraryFlights for an itinerary step.",
+    });
+  }
 };
 
-// Find all itinerarySteps for a itinerary and include the Hotels
-exports.findAllForItineraryWithHotels = (req, res) => {
+exports.findAllForItineraryWithHotels = async (req, res) => {
   const itineraryId = req.query.itineraryId;
-  ItineraryStep.findAll({
-    where: { itineraryId: itineraryId },
-    include: [
-      {
-        model: ItineraryHotel,
-        as: "itineraryHotel",
-        required: false,
-        include: [
-          {
-            model: Hotel,
-            as: "hotel",
-            required: false,
-          }
-        ]
-      },
-    ],
-    order: [["stepNumber", 'ASC']],
-  })
-    .then((data) => {
-      res.send(data);
-    })
-    .catch((err) => {
-      res.status(500).send({
-        message:
-          err.message ||
-          "Some error occurred while retrieving itineraryActivities for a itinerary step.",
-      });
+  try {
+    const data = await ItineraryStep.findAll({
+      where: { itineraryId: itineraryId },
+      include: [
+        {
+          model: ItineraryHotel,
+          as: "itineraryHotel",
+          required: false,
+          include: [
+            {
+              model: Hotel,
+              as: "hotel",
+              required: false,
+            },
+          ],
+        },
+      ],
+      order: [["stepNumber", "ASC"]],
     });
+    res.send(data);
+  } catch (err) {
+    res.status(500).send({
+      message:
+        err.message ||
+        "Some error occurred while retrieving itineraryActivities for an itinerary step.",
+    });
+  }
 };
 
-// Find a single itineraryStep with an id
-exports.findOne = (req, res) => {
+exports.findOne = async (req, res) => {
   const id = req.params.id;
-
-  ItineraryStep.findByPk(id)
-    .then((data) => {
-      res.send(data);
-    })
-    .catch((err) => {
-      res.status(500).send({
-        message:
-          err.message || "Error retrieving itineraryStep with id=" + id,
-      });
+  try {
+    const data = await ItineraryStep.findByPk(id);
+    res.send(data);
+  } catch (err) {
+    res.status(500).send({
+      message: err.message || "Error retrieving itineraryStep with id=" + id,
     });
+  }
 };
 
-// Update a itineraryStep by the id in the request
-exports.update = (req, res) => {
+exports.update = async (req, res) => {
   const id = req.params.id;
-
-  ItineraryStep.update(req.body, {
-    where: { id: id },
-  })
-    .then((number) => {
-      if (number == 1) {
-        res.send({
-          message: "itineraryStep was updated successfully.",
-        });
-      } else {
-        res.send({
-          message: `Cannot update itineraryStep with id=${id}. Maybe itineraryStep was not found or req.body is empty!`,
-        });
-      }
-    })
-    .catch((err) => {
-      res.status(500).send({
-        message: err.message || "Error updating itineraryStep with id=" + id,
-      });
+  try {
+    const [number] = await ItineraryStep.update(req.body, {
+      where: { id: id },
     });
-};
-
-// Delete an itineraryStep with the specified id in the request
-exports.delete = (req, res) => {
-  const id = req.params.id;
-
-  ItineraryStep.destroy({
-    where: { id: id },
-  })
-    .then((number) => {
-      if (number == 1) {
-        res.send({
-          message: "itineraryStep was deleted successfully!",
-        });
-      } else {
-        res.send({
-          message: `Cannot delete itineraryStep with id=${id}. Maybe itineraryStep was not found!`,
-        });
-      }
-    })
-    .catch((err) => {
-      res.status(500).send({
-        message:
-          err.message || "Could not delete itineraryStep with id=" + id,
-      });
-    });
-};
-
-// Delete all itinerarySteps from the database.
-exports.deleteAll = (req, res) => {
-  ItineraryStep.destroy({
-    where: {},
-    truncate: false,
-  })
-    .then((number) => {
+    if (number === 1) {
+      res.send({ message: "itineraryStep was updated successfully." });
+    } else {
       res.send({
-        message: `${number} itinerarySteps were deleted successfully!`,
+        message: `Cannot update itineraryStep with id=${id}. Maybe itineraryStep was not found or req.body is empty!`,
       });
-    })
-    .catch((err) => {
-      res.status(500).send({
-        message:
-          err.message ||
-          "Some error occurred while removing all itinerarySteps.",
-      });
+    }
+  } catch (err) {
+    res.status(500).send({
+      message: err.message || "Error updating itineraryStep with id=" + id,
     });
+  }
+};
+
+exports.delete = async (req, res) => {
+  const id = req.params.id;
+  try {
+    const number = await ItineraryStep.destroy({
+      where: { id: id },
+    });
+    if (number === 1) {
+      res.send({ message: "itineraryStep was deleted successfully!" });
+    } else {
+      res.send({
+        message: `Cannot delete itineraryStep with id=${id}. Maybe itineraryStep was not found!`,
+      });
+    }
+  } catch (err) {
+    res.status(500).send({
+      message: err.message || "Could not delete itineraryStep with id=" + id,
+    });
+  }
+};
+
+exports.deleteAll = async (req, res) => {
+  try {
+    const number = await ItineraryStep.destroy({
+      where: {},
+      truncate: false,
+    });
+    res.send({
+      message: `${number} itinerarySteps were deleted successfully!`,
+    });
+  } catch (err) {
+    res.status(500).send({
+      message:
+        err.message ||
+        "Some error occurred while removing all itinerarySteps.",
+    });
+  }
 };
